31feca9575ea9a411eb3355d2c62941aa262d654
adir-avraham <adir.avraham@gmail.com>
2020-01-17 16:27:19 +0200

    delete-modal

---
+++ client/src/components/delete-dialog/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import { TransitionProps } from "@material-ui/core/transitions";
+
+import { connect } from "react-redux";
+import { deleteHolidayAction } from "../../redux/actions";
+
+const Transition = React.forwardRef<unknown, TransitionProps>(
+  function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  }
+);
+
+export function DeleteDialog(props: any) {
+  const { open, onClose, holidayId } = props;
+  const { deleteHoliday } = props.reduxActions;
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={onClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {"Are you sure do you want do delete this holiday?"}
+        </DialogTitle>
+        <DialogActions>
+          <Button color="primary" onClick={onClose}>
+            Cancel
+          </Button>
+          <Button color="primary"
+            onClick={() => {
+              deleteHoliday(holidayId);
+              onClose();
+            }}
+          >
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    reduxActions: {
+      deleteHoliday: (holidayId: number) => {
+        dispatch(deleteHolidayAction(holidayId));
+      }
+    }
+  };
+};
+
+export default connect(null, mapDispatchToProps)(DeleteDialog);
--- client/src/components/holiday/index.tsx
+++ client/src/components/holiday/index.tsx
@@ -1,137 +1,111 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import IconButton from '@material-ui/core/IconButton';
-import mainAxion from '../axios/mainAxios';
-import { connect } from "react-redux";
-import { deleteHolidayAction } from '../../redux/actions';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 
-const updateFollowHolidayUrl = ('http://localhost:4000/updateFollowStatus')
+import { useState } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteForeverOutlinedIcon from "@material-ui/icons/DeleteForeverOutlined";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import DeleteDialog from "components/delete-dialog";
 
-
-export function Holiday(props: any) {
+export default function Holiday(props: any) {
   const classes = useStyles();
+  const { id, destination, from, to, price, picture, followers, user_id } = props;
+  const [holidayId, setHolidayId] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const { deleteHoliday } = props.reduxActions;
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-
-  const { id, destination, from, to, price, picture, followers, user_id } = props
-    return (
+  return (
     <React.Fragment>
-    <Grid item key={`holiday_${id}`} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image={`${picture}`}
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {destination}
-                    </Typography>
-                    <Typography>
-                     Price: {price}
-                    </Typography>
-                    <Typography>
-                     Dates: {from} - {to}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-
-                    <Button id={id} size="small" color="primary"  
-                    onClick={()=>{  
-                         console.log(id) 
-                        //setHolidayId(id)
-                        deleteHoliday(parseInt(id))
-                        }}
-                    > 
-                    <DeleteForeverOutlinedIcon/>
-                    {/* <DeleteForeverIcon/> */}
-                    </Button>
-                    <Button size="small" color="primary" >
-                    <EditOutlinedIcon/>
-                    {/* <EditIcon/> */}
-                      
-                    </Button>
-                  </CardActions>
-            </Card>
-        </Grid>
+      <Grid item key={`holiday_${id}`} xs={12} sm={6} md={4}>
+        <Card className={classes.card}>
+          <CardMedia className={classes.cardMedia} image={`${picture}`} title="Image title"/>
+          <CardContent className={classes.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {destination}
+            </Typography>
+            <Typography>Price: {price}</Typography>
+            <Typography>
+              Dates: {from} - {to}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button id={id} size="small" color="primary"
+              onClick={() => {
+                setHolidayId(id);
+                handleClickOpen();
+              }}
+            >
+              {/* <DeleteForeverOutlinedIcon /> */}
+              <DeleteForeverIcon/>
+            </Button>
+            <DeleteDialog
+              open={open}
+              onClose={handleClose}
+              holidayId={holidayId}
+            />
+            <Button id={id} size="small" color="primary" 
+              onClick={() => {
+                setHolidayId(id);
+                handleClickOpen();
+              }}
+            >
+              {/* <EditOutlinedIcon /> */}
+              <EditIcon/>
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
     </React.Fragment>
   );
 }
 
-interface State {
-  holidays: Array<object>;
-}
-
-const mapStateToProps = (state: State) => {
-  let { holidays } = state;
-      return { holidays };
-  }   
 
 
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-      reduxActions: {
-        deleteHoliday: (holidayId: number) => {
-          dispatch(deleteHolidayAction(holidayId));
-        }
-
-      }
-  };
-};
-
-export default connect(null, mapDispatchToProps) (Holiday);
-
-
-  
-  const useStyles = makeStyles(theme => ({
-    icon: {
-      marginRight: theme.spacing(2),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
-    },
-  }));
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardMedia: {
+    paddingTop: "56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6)
+  }
+}));
