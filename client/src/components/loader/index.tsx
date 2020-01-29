106a8be8167e1459be015c2299abb68326134108
adir-avraham <adir.avraham@gmail.com>
2020-01-29 12:48:28 +0200

    layout loadder

--- client/src/components/loader/index.tsx
+++ client/src/components/loader/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { Container, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,10 +18,14 @@ export default function LinearIndeterminate() {
   const classes = useStyles();
 
   return (
+    <Container component="main" maxWidth="xs">
     <div className={classes.root}>
-      <LinearProgress />
-      <LinearProgress color="secondary" />
-      <img src="https://static.tapjets.com/media/images/gogo-loader-plane-transparent.gif"/>
+      {/* <LinearProgress />
+      <LinearProgress color="secondary" /> */}
+
+      <img src="https://assetmaritime.in/wp-content/uploads/2018/12/loading.gif"/>
+    
     </div>
+    </Container>
   );
 }
